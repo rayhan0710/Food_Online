b5e17c9428a178ee4f31653ee15a200b1fa73a29
Rayhan <mdrayhan.ahmed.info@gmail.com>
2024-09-20 02:02:39 +0600

    Reset password configaration

--- accounts/views.py
+++ accounts/views.py
@@ -201,7 +201,37 @@ def forgot_password(request):
     return render(request, 'accounts/forgot_password.html')
 
 def reset_password_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.info(request, 'Please reset your password')
+        return redirect('reset_password')
+    else:
+        messages.error(request, 'This link has been expired!')
+        return redirect('myAccount') 
+
     return
 
 def reset_password(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            pk = request.session.get('uid')
+            user = User.objects.get(pk = pk)
+            user.set_password(password)
+            user.is_active = True
+            user.save()
+            messages.success(request, 'Password reset successfully')
+            return redirect('login')
+
+        else:
+            messages.error(request, "Password don't match")
+            return redirect('reset_password')
     return render(request, 'accounts/reset_password.html')
